89d811bff4ec7680f9ebabc3fbacecc55d15308a
Wouter Hermans <wouter.hermans@gmail.com>
2015-09-10 15:17:38 +0000

    fixed dictionary plugin

--- modules/plugins/wtf.py
+++ modules/plugins/wtf.py
@@ -12,27 +12,32 @@ class WtfPlugin(Plugin):
             self.wtf(message.split("!wtf ")[1])
 
     def wtf(self, query):
-        resp = ("I don't know what {0} means".format(query))
-        try:
-            answer = self.lookup(query)
-            resp = "{0}: {1}".format(query, answer)
-        except ValueError:
-            pass
-        except HTTPError:
-            pass
+        if len(query.split()) > 1:
+            resp = "I'm sorry, I only understand single words"
+        else:
+            resp = ("I don't know what {0} means".format(query))
+            try:
+                answer = self.lookup(query)
+                if answer != "":
+                    resp = "{0}: {1}".format(query, answer)
+            except ValueError:
+                pass
+            except HTTPError:
+                pass
         self.message(resp)
 
     def lookup(self, query):
         answer = ""
         query= ''.join(c for c in query if c.isalnum())
         url = "http://api.wordnik.com:80/v4/word.json/{0}/definitions?limit=1&includeRelated=true&sourceDictionaries=all&useCanonical=true&includeTags=false&api_key={1}".format(query, self.cfg.api_key_1)
-        print("Looking up {0}".format(url))
         try:
-            reply = json.load(self.request(url))
-            answer = reply.text
+            reply = json.loads(self.fetch(url))
+            if len(reply) > 0 and 'text' in reply[0]:
+                answer = reply[0]['text']
         except ValueError:
             pass
         return answer
 
-    def request(self, url):
-        resp = urlopen(url)
+    def fetch(self, url):
+        resp = urlopen(url).read()
+        return str(resp, 'utf-8')
